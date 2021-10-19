04e39322b6ffe156a314eef7749326da968c2727
gerard <gerardfp@gmail.com>
2021-10-19 09:51:59 +0000

    dbdata

---
+++ wordpress_data/wp-content/plugins/myplugin/myplugin.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Plugin Name: My First Plugin
+ * Plugin URI: http://www.mywebsite.com/my-first-plugin
+ * Description: The very first plugin that I have ever created.
+ * Version: 1.0
+ * Author: Your Name
+ * Author URI: http://www.mywebsite.com
+ */
+
+add_action( 'the_content', 'my_thank_you_text' );
+
+function my_thank_you_text ( $content ) {
+    return $content .= '<p>Thank you for reading!</p>';
+}
+
+?>
